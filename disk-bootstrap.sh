2a7c868b863d2a711739de8b71377168ded2fca4
alulord <alulord@gmail.com>
2020-09-19 08:14:34 +0000

    add efivars to systemd-nspawn

--- disk-bootstrap.sh
+++ disk-bootstrap.sh
@@ -9,6 +9,7 @@ sgdisk --clear \
        --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:cryptswap \
        --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
          "$DRIVE"
+partprobe "$DRIVE"
 
 cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 /dev/disk/by-partlabel/cryptsystem
 cryptsetup open /dev/disk/by-partlabel/cryptsystem system
@@ -31,9 +32,6 @@ mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
 mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
 mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/.snapshots
 mount -o $o LABEL=EFI /mnt/boot
-mount --bind /proc /mnt/proc
-mount --bind /sys /mnt/sys
-mount --bind -t efivars efivars /mnt/sys/firmware/efi/efivars
 
 pacstrap /mnt base base-devel git nano ansible
 genfstab -L -p /mnt >> /mnt/etc/fstab
@@ -45,5 +43,6 @@ echo "root_uuid: $(blkid -t LABEL=system -s UUID -o value)" >> /install/group_va
 echo "pts/0" >> /mnt/etc/securetty
 cp -r /install /mnt/
 systemd-nspawn -D /mnt
-systemd-nspawn -bD /mnt
+systemd-nspawn --bind /sys/firmware/efi/efivars -bD /mnt
 #rm -rf /mnt/install
+echo "All done, reboot system"
