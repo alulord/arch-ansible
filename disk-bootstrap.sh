afa760935dd67909e81fc03c548f6f111fc4a68b
Peter <alulord@gmail.com>
2020-10-01 17:52:45 +0200

    add support for specific disk size

--- disk-bootstrap.sh
+++ disk-bootstrap.sh
@@ -2,13 +2,17 @@
 set -e
 
 DRIVE="$1"
+DISK_SIZE="$2"
+if [[ -n "$DISK_SIZE" ]]; then
+  DISK_SIZE="+$DISK_SIZE"
+fi
 
 sgdisk --zap-all --clear $DRIVE
 sgdisk --clear \
-       --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
-       --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:cryptswap \
-       --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
-         "$DRIVE"
+  --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
+  --new=2:0:+8GiB --typecode=2:8200 --change-name=2:cryptswap \
+  --new=3:0:"${DISK_SIZE:-0}" --typecode=3:8300 --change-name=3:cryptsystem \
+  "$DRIVE"
 partprobe "$DRIVE"
 
 cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 /dev/disk/by-partlabel/cryptsystem
@@ -34,13 +38,13 @@ mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/.snapshots
 mount -o $o LABEL=EFI /mnt/boot
 
 pacstrap /mnt base base-devel git nano ansible
-genfstab -L -p /mnt >> /mnt/etc/fstab
+genfstab -L -p /mnt >>/mnt/etc/fstab
 sed -i "s+LABEL=swap+/dev/mapper/swap+" /mnt/etc/fstab
 
 mkdir /mnt/install
-echo "luks_uuid: $(blkid -t PARTLABEL=cryptsystem -s UUID -o value)" >> /install/group_vars/all.yaml
-echo "root_uuid: $(blkid -t LABEL=system -s UUID -o value)" >> /install/group_vars/all.yaml
-echo "pts/0" >> /mnt/etc/securetty
+echo "luks_uuid: $(blkid -t PARTLABEL=cryptsystem -s UUID -o value)" >>/install/group_vars/all.yaml
+echo "root_uuid: $(blkid -t LABEL=system -s UUID -o value)" >>/install/group_vars/all.yaml
+echo "pts/0" >>/mnt/etc/securetty
 cp -r /install /mnt/
 systemd-nspawn -D /mnt
 systemd-nspawn --bind /sys/firmware/efi/efivars -bD /mnt
